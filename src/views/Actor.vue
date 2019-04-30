c90ef0e978fb5606ad1dd34cdac3df3ab8abf0b2
ActiveChai <1078486770@qq.com>
2019-04-30 16:22:11 +0800

    actor界面ok

--- src/views/Actor.vue
+++ src/views/Actor.vue
@@ -1,14 +1,45 @@
 <template>
-    <div
-        class="main-actor"
-    >actoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractoractor</div>
+    <div class="main-actor">
+        <el-card class="box-card">
+            <template v-for="item in actor">
+                <div slot="header" class="clearfix" :key="item.name">
+                    <span>{{item.name}}</span>
+                </div>
+                <div class="body" :key="item.name">
+                    <img
+                        class="img"
+                        src="https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p28576.webp"
+                    >
+                    <div class="actor">
+                        <div class="text item">{{'性别：'+item.sex}}</div>
+                        <div class="text item">{{'出生日期：'+item.birthday}}</div>
+                        <div class="text item">{{'出生地：'+item.birthplace}}</div>
+                        <div class="text item">{{'个人简介：'+item.summary}}</div>
+                    </div>
+                </div>
+            </template>
+        </el-card>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'actor',
     data() {
-        return {}
+        return {
+            actor: [
+                {
+                    name: '黄圣依',
+                    photo:
+                        'https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p28576.webp',
+                    sex: '女',
+                    birthday: '1983/2/11',
+                    birthplace: '中国,上海',
+                    summary:
+                        '黄圣依，中国内地知名影视女演员、歌手，曾签约星辉公司，凭借电影《功夫》迅速成为国内颇具代表性的一线女星。1983年2月11日出生于上海，2005年毕业于北京电影学院表演系。在2009年4月的“80后新生代娱乐大明星”评选活动中，黄圣依与刘亦菲、杨幂、王珞丹，被评为内地新“四小花旦”。2011年6月13日在第十四届上海国际电影节上宣布已成立“巨力圣依影视传媒有限公司”，自行管理公司。明星身份之外，黄圣依现已成为中国巨力影视传媒有限公司执行总裁、巨力圣依影视传媒有限公司总裁。'
+                }
+            ]
+        }
     }
 }
 </script>
